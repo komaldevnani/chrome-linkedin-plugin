38dba0162fdaf6222c453e8d592fbb1c54c9e1ff
komal devnani <komaldevnani00@gmail.com>
2020-10-29 23:04:51 +0530

    post to api

--- linkedin-ext/content.js
+++ linkedin-ext/content.js
@@ -7,11 +7,33 @@ function getName() {
 
 
 function getCompany() {
-    let company_name = document.getElementsByClassName("pv-top-card--experience-list-item")
-    company_name = company_name[0].innerText
-    return company_name
+    if (document.getElementsByClassName("pv-top-card--experience-list-item").length) {
+        let company_name = document.getElementsByClassName("pv-top-card--experience-list-item")
+        company_name = company_name[0].innerText
+        return company_name
+    }
 }
 
 
-console.log(getName())
-console.log(getCompany())
+let name = getName().split(" ")
+let company = getCompany()
+let url = "http://localmost:3000/users"
+
+
+fetch(url,{
+        method: 'POST',
+        body: JSON.stringify({
+            first_name: name[0],
+            last_name: name[1],
+            company_name: company
+            }),
+        headers: {
+            "content-type": "application/json; charset=UTF-8"
+        }
+}).then(() => {
+    console.log("saved to API");
+}).catch(err => console.log(err));
+
+
+
+
